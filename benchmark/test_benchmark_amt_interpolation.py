900e8690351ed2f5a3be0d08bc91f4523d41ead4
Licheng <ll6@illinois.edu>
2021-09-05 14:31:14 -0500

    Fix errors.

--- benchmark/test_benchmark_amt_interpolation.py
+++ benchmark/test_benchmark_amt_interpolation.py
@@ -5,7 +5,7 @@ from random import randint
 from honeybadgermpc.betterpairing import ZR, G1
 from honeybadgermpc.polynomial import polynomials_over
 from honeybadgermpc.poly_commit_log import PolyCommitLog
-from honeybadgermpc.poly_commit_dummy import PolyCommitAMTDummy, PolyCommitLoglinDummy, SimulatedPclProof, SimulatedPclCom, SimulatedAMTProof, SimulatedAMTCom, ActualAMTCom, ActualAMTProof
+from honeybadgermpc.poly_commit_dummy import PolyCommitAMTDummy, PolyCommitLoglinDummy, SimulatedPclProof, SimulatedPclCom, SimulatedAMTProof, SimulatedAMTCom, ActualAMTCom, ActualAMTProof, log2, findNextPowerOf2
 from honeybadgermpc.field import GF
 from honeybadgermpc.utils.misc import print_exception_callback, wrap_send, subscribe_recv
 from honeybadgermpc.router import SimpleRouter
@@ -15,6 +15,7 @@ import logging
 import time
 import cProfile
 from honeybadgermpc.share_recovery import poly_lagrange_at_x, poly_interpolate_at_x, interpolate_g1_at_x
+from honeybadgermpc.poly_commit_amt_unity import PolyCommitAMTUnity, get_all_roots_of_unity, bit_reverse, gen_crs
 
 short_param_list_t = [1,
                       2,
@@ -54,8 +55,9 @@ def test_amt_commit_and_proof_interpolation(benchmark_router, benchmark, t):
     r = ZR.random()
     for k in range(secret_count):
         phis[k] = poly.random(t, values[k])
-        commitments[k] = poly_commit.commit(phis[k], r)
-    witnesses = pc.double_batch_create_witness(phis, r)[1]
+        commitments[k] = pc.commit(phis[k])
+    
+    witnesses = [[pc.create_witness(phis[k], j) for i in range(len(phis))] for j in range(n)][1]
 
     params = (t, n, values, commitments, witnesses)
     def _prog():
